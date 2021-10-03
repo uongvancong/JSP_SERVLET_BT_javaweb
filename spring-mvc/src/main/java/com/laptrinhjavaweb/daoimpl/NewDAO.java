131d48f973803805f4703955de7ee43dad240bea
uông văn công <uongvancong@gmail.com>
2021-10-03 22:04:32 +0700

    03/10 - update

---
+++ spring-mvc/src/main/java/com/laptrinhjavaweb/daoimpl/NewDAO.java
@@ -0,0 +1,20 @@
+package com.laptrinhjavaweb.daoimpl;
+
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.laptrinhjavaweb.dao.INewDAO;
+import com.laptrinhjavaweb.mapper.NewMapper;
+import com.laptrinhjavaweb.model.NewModel;
+
+@Repository
+public class NewDAO extends AbstractDAO<NewModel> implements INewDAO {
+	
+	@Override
+	public List<NewModel> findAll() {
+		StringBuilder sql = new StringBuilder("SELECT * FROM news");
+		return query(sql.toString(), new NewMapper());
+	}
+}
